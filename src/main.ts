7ae9dd185cccbc9f94cefe20ac4a4e70fedfdd53
Mustafa Yılmaz <deneme@denee.com>
2023-04-08 23:42:21 +0300

    fixed port

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(8080, '0.0.0.0');
 
   console.log('Application is running on: ' + (await app.getUrl()));
 }
