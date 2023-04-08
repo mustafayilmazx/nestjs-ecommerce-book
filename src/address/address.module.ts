355262a21014aefe34cd09dbe1ed7b6d7884245e
Mustafa Yılmaz <deneme@denee.com>
2023-04-09 01:22:05 +0300

    fix

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Address, AddressSchema } from '@schemas/address';
 import { AddressController } from './address.controller';
 import { AddressService } from './address.service';
-import { Address, AddressSchema } from '@schemas/address';
 
 @Module({
   imports: [
